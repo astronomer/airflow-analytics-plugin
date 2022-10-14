641bf925f6e1e19f3d3766b5a7f15cada66b681b
blag <blag@users.noreply.github.com>
2022-10-13 22:01:23 -0700

    Cleanup database query and FAB view

--- src/plugins/analytics_plugin.py
+++ src/plugins/analytics_plugin.py
@@ -1,13 +1,14 @@
 import logging
 from typing import Any
 
-from sqlalchemy import text
+from sqlalchemy import and_, distinct, func, text
+from sqlalchemy.orm import aliased
 
 from flask import Blueprint, request
 from flask_appbuilder import BaseView as AppBuilderBaseView
 from flask_appbuilder import expose
 
-from airflow import configuration
+from airflow import __version__ as airflow_version, configuration
 from airflow.models import Log, TaskInstance
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.session import provide_session
@@ -25,91 +26,67 @@ bp = Blueprint(
     static_folder="static",
     static_url_path="/static/",
 )
-airflow_webserver_base_url = configuration.get("webserver", "BASE_URL")
 
-apis_metadata = [
-    {
-        "name": "tasks",
-        "description": "return number of successful and failed tasks for specified time period",
-        "airflow_version": "xxx",  # airflow.version something like that
-        "http_method": ["GET"],
-        "arguments": [
-            {
-                "name": "startDate",
-                "description": "The start date of the task period (Example: YYYY-MM-DD )",
-                "form_input_type": "text",
-                "required": True,
-            },
-            {
-                "name": "endDate",
-                "description": "The end date of the task period (Example: YYYY-MM-DD)",
-                "form_input_type": "text",
-                "required": True,
-            },
-        ],
-    }
-]
+airflow_webserver_base_url = configuration.get("webserver", "BASE_URL")
 
 
 @provide_session
-def dags_report(session) -> Any:
+def tasks_report(session) -> Any:
     untrusted_start_date = request.args.get("startDate")  # 2022-08-01
     untrusted_end_date = request.args.get("endDate")  # 2022-08-30
     try:
-        parse(untrusted_start_date)
-        parse(untrusted_end_date)
-    except ValueError:
-        log.error("The string is not a date ")
+        start_date = parse(untrusted_start_date)
+        end_date = parse(untrusted_end_date)
+    except ValueError as e:
+        log.error(f"The string is not a date: {e}")
+        raise e
     else:
+        log_success = aliased(Log, name="log_success")
+        log_failed = aliased(Log, name="log_failed")
         # remove the dummy operator and the astronomer_monitoring_dag that is added in runtime from task count
         query = (
             session.query(
-                Log.event,
-                func.count(Log.event).label("totalCount"),
+                func.count(distinct(log_success.id)).label("total_success"),
+                func.count(distinct(log_failed.id)).label("total_failed"),
             )
-            .join(
-                TaskInstance.event.in_(
-                    TaskInstanceState.SUCCESS, TaskInstanceState.FAILED
+            .select_from(TaskInstance)
+            .outerjoin(
+                log_success,
+                and_(
+                    log_success.task_id == TaskInstance.task_id,
+                    log_success.dttm >= start_date,
+                    log_success.dttm <= end_date,
+                    log_success.dag_id != "astronomer_monitoring_dag",
+                    log_success.event == TaskInstanceState.SUCCESS,
                 ),
-                TaskInstance.operator != "DummyOperator",
-                TaskInstance.task_id == Log.task_id,
+                isouter=True,
             )
-            .filter(
-                Log.dttm >= start_date,
-                Log.dttm <= end_date,
-                Log.dag_id != "astronomer_monitoring_dag",
+            .outerjoin(
+                log_failed,
+                and_(
+                    log_failed.task_id == TaskInstance.task_id,
+                    log_failed.dttm >= start_date,
+                    log_failed.dttm <= end_date,
+                    log_failed.dag_id != "astronomer_monitoring_dag",
+                    log_success.event == TaskInstanceState.FAILED,
+                ),
+                isouter=True,
             )
-            .group_by(Log.event)
+            .filter(TaskInstance.operator != "DummyOperator")
         )
 
-        return [dict(result_row) for result_row in session.query(query).all()]
-
-
-def format_db_response(resp):
-    log.info(resp)
-    task_summary_arr = resp["dags_report"]
-
-    key_conversion = {"success": "total_success", "failed": "total_failed"}
-    temp_result = {}
-    for date_task_summary in task_summary_arr:
-        fieldName = key_conversion[date_task_summary["event"]]
-        temp_result[fieldName] = date_task_summary["totalcount"]
-
-    return temp_result
-
-
-rest_api_endpoint = "/astronomeranalytics/api/v1/"
+        return dict(session.query(query).one())
 
 
 def try_reporter(reporter_func):
     try:
         rtn = {
-            reporter_func.__name__: reporter_func(),
+            "results": reporter_func(),
         }
     except Exception as e:
         logging.exception(f"Failed reporting {reporter_func.__name__}")
         rtn = {
-            reporter_func.__name__: str(e),
+            "error": str(e),
         }
     return rtn
 
@@ -123,31 +100,48 @@ class AstronomerAnalytics(AppBuilderBaseView):
         return self.render_template(
             "/analytics_plugin/index.html",
             airflow_webserver_base_url=airflow_webserver_base_url,
-            rest_api_endpoint=rest_api_endpoint,
-            apis_metadata=apis_metadata,
+            rest_api_endpoint="/astronomeranalytics/api/v1/",
+            apis_metadata=[
+                {
+                    "name": "tasks",
+                    "description": "return number of successful and failed tasks for specified time period",
+                    "airflow_version": airflow_version,
+                    "http_method": ["GET"],
+                    "arguments": [
+                        {
+                            "name": "startDate",
+                            "description": "The start date of the task period (Example: YYYY-MM-DD )",
+                            "form_input_type": "text",
+                            "required": True,
+                        },
+                        {
+                            "name": "endDate",
+                            "description": "The end date of the task period (Example: YYYY-MM-DD)",
+                            "form_input_type": "text",
+                            "required": True,
+                        },
+                    ],
+                }
+            ],
         )
 
     @expose("api/v1/tasks")
     def tasks(self):
-        dags = try_reporter(dags_report)
-
-        if dags["dags_report"] == None:
-            return {
-                "message": "Invalid date",
-            }
-        return {f"dags": format_db_response(dags)}
+        return {
+            "tasks": try_reporter(tasks_report),
+        }
 
 
 # Defining the plugin class
 class AstronomerPlugin(AirflowPlugin):
-    name = "AstronomerAnalytics"
+    name = "Astronomer Analytics"
     hooks = []
     macros = []
     flask_blueprints = [bp]
     appbuilder_views = [
         {
-            "name": "API's",
-            "category": "AstronomerAnalytics",
+            "name": "APIs",
+            "category": "Astronomer Analytics",
             "view": AstronomerAnalytics(),
         }
     ]
